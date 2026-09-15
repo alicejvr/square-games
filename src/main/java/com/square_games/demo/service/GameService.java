068b7f90963769ecac3a7a01442bf0966ebec7fe
alicejvr <alicejover@gmail.com>
2026-09-15 14:49:25 +0200

    fix bug itération 2

--- src/main/java/com/square_games/demo/service/GameService.java
+++ src/main/java/com/square_games/demo/service/GameService.java
@@ -5,23 +5,26 @@ import fr.le_campus_numerique.square_games.engine.Game;
 import org.springframework.stereotype.Service;
 
 import java.util.Locale;
+import java.util.Map;
 
 @Service
 public class GameService {
 
 
-    private final GamePlugin gamePlugin;
+    private final Map<String, GamePlugin> gamePluginsByName;
 
-    public GameService(GamePlugin gamePlugin) {
-        this.gamePlugin = gamePlugin;
+    public GameService(Map<String, GamePlugin> gamePluginsByName) {
+        this.gamePluginsByName = gamePluginsByName;
     }
 
-    public Game createGame() {
-        return gamePlugin.createGame();
+    public Game createGame(String gameName) {
+        GamePlugin plugin = gamePluginsByName.get(gameName);
+        return plugin.createGame();
     }
 
-    public String getGameName(Locale locale) {
-        return gamePlugin.getName(locale);
+    public String getGameName(String gameName, Locale locale) {
+        GamePlugin plugin = gamePluginsByName.get(gameName);
+        return plugin.getName(locale);
     }
 
 }
