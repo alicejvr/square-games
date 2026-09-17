bcd848234a1f9740f2cfd81a4e59c3dcd7742962
alicejvr <alicejover@gmail.com>
2026-09-17 13:20:48 +0200

    itération 3 - add JdbcGameDao

--- src/main/java/com/square_games/demo/dao/JdbcGameDao.java
+++ src/main/java/com/square_games/demo/dao/JdbcGameDao.java
@@ -1,4 +1,14 @@
 package com.square_games.demo.dao;
 
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class JdbcGameDao {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+    public JdbcGameDao(NamedParameterJdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 }
