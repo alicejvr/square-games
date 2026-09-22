7233fbc02e3b59a30081b33259eeb5b8ba2e22ff
alicejvr <alicejover@gmail.com>
2026-09-22 16:25:23 +0200

    swagger UI + Springdoc

--- src/main/java/com/square_games/demo/controller/GameCatalogController.java
+++ src/main/java/com/square_games/demo/controller/GameCatalogController.java
@@ -1,6 +1,7 @@
 package com.square_games.demo.controller;
 
 import com.square_games.demo.service.GameCatalog;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class GameCatalogController {
         this.gameCatalog = gameCatalog;
     }
 
+    @Operation(summary = "Récupérer les jeux disponibles")
     @GetMapping("/gameCatalog")
     public Map<String, String> getGames(Locale locale) {
         return gameCatalog.getGames(locale);
--- src/main/java/com/square_games/demo/controller/GameController.java
+++ src/main/java/com/square_games/demo/controller/GameController.java
@@ -5,6 +5,9 @@ import com.square_games.demo.dao.GameDao;
 import com.square_games.demo.service.GameService;
 import fr.le_campus_numerique.square_games.engine.CellPosition;
 import fr.le_campus_numerique.square_games.engine.Game;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -48,6 +51,22 @@ public class GameController {
         return true;
     }
 
+
+    @Operation(summary = "Créer une nouvelle partie")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Partie créée avec succès"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "L'utilisateur n'existe pas"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Paramètres invalides"
+            )
+    })
     @PostMapping("/games")
     public void createGame(@RequestBody GameCreationParams params,
                            @RequestHeader("X-UserId") String userId) {
@@ -63,6 +82,14 @@ public class GameController {
 
         }
 
+
+    @Operation(summary = "Récupérer toutes les parties")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Liste des ID des parties récupérée avec succès"
+            )
+    })
     @GetMapping("/games")
     public Stream<@NotNull UUID> getAllGame() {
 
@@ -71,6 +98,18 @@ public class GameController {
         return gameDao.findAll().map(Game::getId);
     }
 
+
+    @Operation(summary = "Récupérer les parties d'un user")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Liste des parties de l'utilisateur récupérée avec succès"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "L'utilisateur n'existe pas"
+            )
+    })
     @GetMapping("/gamesForUser")
     public Stream<@NotNull UUID> getAllGame(@RequestHeader("X-UserId") String userId) {
 
@@ -80,6 +119,22 @@ public class GameController {
         return gameDao.findByPlayerId(UUID.fromString(userId)).map(Game::getId);
     }
 
+
+    @Operation(summary = "Récupérer une partie")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Partie récupérée avec succès"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "L'utilisateur n'existe pas"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "La partie demandée n'existe pas"
+            )
+    })
     @GetMapping("/games/{gameId}")
     public Optional<Game> getGame(@PathVariable UUID gameId,
                                   @RequestHeader("X-UserId") String userId) {
@@ -92,6 +147,22 @@ public class GameController {
         return gameDao.findById(String.valueOf(gameId)); // valueOf convertit l'UUID en String
     }
 
+
+    @Operation(summary = "Récupérer les moves possibles d'un pion")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Liste des mouvements possibles récupérée avec succès"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "L'utilisateur n'existe pas"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "La partie ou le pion demandé n'existe pas"
+            )
+    })
     @GetMapping("/games/{gameId}/tokens/{tokenId}/moves")
     public Set<CellPosition> getPossibleMoves(@PathVariable String gameId,
                                               @PathVariable String tokenId,
@@ -107,6 +178,25 @@ public class GameController {
     }
 
 
+    @Operation(summary = "Jouer un move")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Mouvement effectué avec succès"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "L'utilisateur n'est pas autorisé à effectuer ce mouvement"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "La partie demandée n'existe pas"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "La position demandée est invalide"
+            )
+    })
     @PostMapping("/games/{gameId}/moves")
     public void playMove(@PathVariable String gameId,
                          @RequestBody CellPosition position,
